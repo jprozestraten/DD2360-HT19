ca65df7588eee7e1166208894c4b942b328433dc
SamirS97 <samirsayah97@outlook.fr>
2019-12-03 16:23:53 +0100

    pushing the right files

--- Assignment_3/ex_2/exercise_2a.cu
+++ Assignment_3/ex_2/exercise_2a.cu
@@ -12,9 +12,7 @@ struct Particle
 
 __host__ __device__ uint gen_random(uint a, uint b, uint c = 10, uint seed = 10)
 {
-	//printf("RANDOM NUMBER GENERATED : %d \n",(seed*a+b) % c);
 	return (seed*a+b) % c;
-	//return 2;
 }
 
 __device__ void update_position(Particle* part_array_gpu, float dt, uint i)
@@ -35,23 +33,16 @@ __device__ void update_velocity(Particle* part_array_gpu, float dt, uint i, uint
 __global__ void updateKernel(Particle* part_array_gpu, float dt, uint j)
 {
 	const int i = blockIdx.x*blockDim.x + threadIdx.x;
-	//printf("GPU : %.4f , je suis le thread %d \n", testgpu, i);
 	update_velocity(part_array_gpu, dt, i, j);
 	update_position(part_array_gpu, dt, i);
-	//printf("random_num gpu : %.4f \n", (float)(gen_random(i, j)));
-	float testgpu = part_array_gpu[i].position.x;
 }
 
 __host__ void updateCPU(Particle* part_array_cpu, float dt, uint j, uint NPARTICLES)
 {
-	float testcpu = 1.0;
 	for (int i = 0; i < NPARTICLES; i++)
 	{
 		if(i < NPARTICLES)
-		{
-			testcpu = part_array_cpu[i].position.x;
-			//printf("CPU : %.4f \n", testcpu);
-			//printf("random_num cpu : %.d \n", gen_random(i, j));
+		{	
 			part_array_cpu[i].velocity.x += (float)(gen_random(i, j)) * dt;
 			part_array_cpu[i].velocity.y += (float)(gen_random(i, j)) * dt;
 			part_array_cpu[i].velocity.z += (float)(gen_random(i, j)) * dt;
@@ -104,10 +95,8 @@ int main(int argc, char** argv)
 	Particle* particles_cpu;
 	Particle* particles_res;
 
-	//particles_cpu = new Particle[NPARTICLES];
-	//particles_res = new Particle[NPARTICLES];
-	cudaMalloc((void **) &particles_cpu, NPARTICLES*sizeof(Particle), cudaHostAllocDefault);
-	cudaMalloc((void **) &particles_res, NPARTICLES*sizeof(Particle), cudaHostAllocDefault);
+	cudaHostAlloc((void **) &particles_cpu, NPARTICLES*sizeof(Particle), cudaHostAllocDefault);
+	cudaHostAlloc((void **) &particles_res, NPARTICLES*sizeof(Particle), cudaHostAllocDefault);
 
 	// Allocate device memory to store the output array
 	cudaMalloc(&particles_gpu, NPARTICLES*sizeof(Particle));
@@ -152,7 +141,6 @@ int main(int argc, char** argv)
 	cudaDeviceSynchronize();
 	//double iElaps_gpu = cpuSecond() - iStart_gpu;
 
-	float testcpu = 1.0, testgpu = 1.0;
 
 	//double iStart_cpu = cpuSecond();
 	//CPU computation
@@ -166,8 +154,6 @@ int main(int argc, char** argv)
 	for (int i = 0; i < NPARTICLES; i++) {
 		if(i < NPARTICLES)
 		{
-			testcpu = particles_cpu[i].position.x; 
-			testgpu = particles_res[i].position.x;
 			if(!(compareParticle(particles_cpu[i], particles_res[i]))) {
 				flag = 0;
 				break;
@@ -189,8 +175,8 @@ int main(int argc, char** argv)
 	//delete[] particles_cpu;
 	//delete[] particles_res;
 	cudaFree(particles_gpu);
-	cudaFree(particles_gpu);
-	cudaFree(particles_res); // Free the memory
+	cudaFreeHost(particles_gpu);
+	cudaFreeHost(particles_res); // Free the memory
 
 	return 0;
 }
--- Assignment_3/ex_2/exercise_2b.cu
+++ Assignment_3/ex_2/exercise_2b.cu
@@ -12,9 +12,7 @@ struct Particle
 
 __host__ __device__ uint gen_random(uint a, uint b, uint c = 10, uint seed = 10)
 {
-	//printf("RANDOM NUMBER GENERATED : %d \n",(seed*a+b) % c);
 	return (seed*a+b) % c;
-	//return 2;
 }
 
 __device__ void update_position(Particle* part_array_gpu, float dt, uint i)
@@ -35,23 +33,16 @@ __device__ void update_velocity(Particle* part_array_gpu, float dt, uint i, uint
 __global__ void updateKernel(Particle* part_array_gpu, float dt, uint j)
 {
 	const int i = blockIdx.x*blockDim.x + threadIdx.x;
-	//printf("GPU : %.4f , je suis le thread %d \n", testgpu, i);
 	update_velocity(part_array_gpu, dt, i, j);
 	update_position(part_array_gpu, dt, i);
-	//printf("random_num gpu : %.4f \n", (float)(gen_random(i, j)));
-	float testgpu = part_array_gpu[i].position.x;
 }
 
 __host__ void updateCPU(Particle* part_array_cpu, float dt, uint j, uint NPARTICLES)
 {
-	float testcpu = 1.0;
 	for (int i = 0; i < NPARTICLES; i++)
 	{
 		if(i < NPARTICLES)
 		{
-			testcpu = part_array_cpu[i].position.x;
-			//printf("CPU : %.4f \n", testcpu);
-			//printf("random_num cpu : %.d \n", gen_random(i, j));
 			part_array_cpu[i].velocity.x += (float)(gen_random(i, j)) * dt;
 			part_array_cpu[i].velocity.y += (float)(gen_random(i, j)) * dt;
 			part_array_cpu[i].velocity.z += (float)(gen_random(i, j)) * dt;
@@ -100,17 +91,21 @@ int main(int argc, char** argv)
 	const int NPARTICLES = atoi(argv[3]);
 
 	// Declare a pointer for an array of particles
+	
 	Particle* particles_gpu;
 	Particle* particles_cpu;
-	Particle* particles_res;
+	//Particle* particles_res; 
 
 	//particles_cpu = new Particle[NPARTICLES];
 	//particles_res = new Particle[NPARTICLES];
 	cudaHostAlloc((void **) &particles_cpu, NPARTICLES*sizeof(Particle), cudaHostAllocDefault);
-	cudaHostAlloc((void **) &particles_res, NPARTICLES*sizeof(Particle), cudaHostAllocDefault);
+	//cudaHostAlloc((void **) &particles_res, NPARTICLES*sizeof(Particle), cudaHostAllocDefault); 
 
 	// Allocate device memory to store the output array
-	cudaMalloc(&particles_gpu, NPARTICLES*sizeof(Particle));
+	//cudaMalloc(&particles_gpu, NPARTICLES*sizeof(Particle)); 
+
+	// Allocate managed memory
+	cudaMallocManaged(&particles_gpu, NPARTICLES*sizeof(Particle));
 
 	for (int i = 0; i < NPARTICLES; i++)
 	{
@@ -124,13 +119,13 @@ int main(int argc, char** argv)
 			particles_cpu[i].position.y = 1;
 			particles_cpu[i].position.z = 1;
 
-			particles_res[i].velocity.x = 0;
-			particles_res[i].velocity.y = 0;
-			particles_res[i].velocity.z = 0;
+			particles_gpu[i].velocity.x = 0;
+			particles_gpu[i].velocity.y = 0;
+			particles_gpu[i].velocity.z = 0;
 
-			particles_res[i].position.x = 1;
-			particles_res[i].position.y = 1;
-			particles_res[i].position.z = 1;
+			particles_gpu[i].position.x = 1;
+			particles_gpu[i].position.y = 1;
+			particles_gpu[i].position.z = 1;
 
 		}
 	}
@@ -145,14 +140,13 @@ int main(int argc, char** argv)
 	for (int j = 0; j < NITER; j++) 
 	{
 		//printf("GPU iteration numéro %d : \n", j);
-		cudaMemcpy(particles_gpu, particles_res, NPARTICLES*sizeof(Particle), cudaMemcpyHostToDevice);
+		//cudaMemcpy(particles_gpu, particles_res, NPARTICLES*sizeof(Particle), cudaMemcpyHostToDevice);
 		updateKernel<<<(NPARTICLES+TPB-1) / TPB, TPB>>>(particles_gpu, dt, j);
-		cudaMemcpy(particles_res, particles_gpu, NPARTICLES*sizeof(Particle), cudaMemcpyDeviceToHost);
+		//cudaMemcpy(particles_res, particles_gpu, NPARTICLES*sizeof(Particle), cudaMemcpyDeviceToHost);
 	}
 	cudaDeviceSynchronize();
 	//double iElaps_gpu = cpuSecond() - iStart_gpu;
 
-	float testcpu = 1.0, testgpu = 1.0;
 
 	//double iStart_cpu = cpuSecond();
 	//CPU computation
@@ -166,9 +160,7 @@ int main(int argc, char** argv)
 	for (int i = 0; i < NPARTICLES; i++) {
 		if(i < NPARTICLES)
 		{
-			testcpu = particles_cpu[i].position.x; 
-			testgpu = particles_res[i].position.x;
-			if(!(compareParticle(particles_cpu[i], particles_res[i]))) {
+			if(!(compareParticle(particles_cpu[i], particles_gpu[i]))) {
 				flag = 0;
 				break;
 			}
@@ -190,7 +182,7 @@ int main(int argc, char** argv)
 	//delete[] particles_res;
 	cudaFree(particles_gpu);
 	cudaFreeHost(particles_gpu);
-	cudaFreeHost(particles_res); // Free the memory
+	//cudaFreeHost(particles_res); // Free the memory
 
 	return 0;
 }
